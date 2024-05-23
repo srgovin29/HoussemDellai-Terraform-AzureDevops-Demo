a6a8d457c5290643abd98619de2f43bc0743946f
Srinivasan <srgovin29@outlook.com>
2024-05-23 05:52:05 +0530

    working one

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ The video explaining the different steps is available here:
 <img width="60%" src="https://i.ytimg.com/vi/ukmbiTSWU_M/maxresdefault.jpg">
 <a href="https://youtu.be/ukmbiTSWU_M">https://youtu.be/ukmbiTSWU_M</a>
 </img>
+
+# Srinivasan Notes
+
+the Skip-Step was originaaly from author and it was not working due to outdated commands
+So use main.tf file from the root directory
--- main.tf
+++ main.tf
@@ -1,17 +1,51 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
-  required_version = ">= 1.1.0"
-}
 provider "azurerm" {
   features {}
 }
-resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
+
+resource "azurerm_resource_group" "RG-Terraform" {
+  name     = "terraform-resource-group1"
   location = "Southeast Asia"
+}
+
+resource "azurerm_service_plan" "ASP-Terraform" {
+  name                = "terraform-appserviceplan"
+  location            = azurerm_resource_group.RG-Terraform.location
+  resource_group_name = azurerm_resource_group.RG-Terraform.name
+  os_type             = "Linux"
+  sku_name            = "S1"
+}
+
+resource "azurerm_linux_web_app" "AS-Terraform" {
+  name                = "app-service-terraform-srgovin"
+  resource_group_name = azurerm_resource_group.RG-Terraform.name
+  location            = azurerm_resource_group.RG-Terraform.location
+  service_plan_id     = azurerm_service_plan.ASP-Terraform.id
+
+  site_config {
+  
+  }
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=tcp:${azurerm_mssql_server.terraform-sqlserver.fully_qualified_domain_name} Database=${azurerm_mssql_database.terraform-sqldatabase.name};User ID=${azurerm_mssql_server.terraform-sqlserver.administrator_login};Password=${azurerm_mssql_server.terraform-sqlserver.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
+   # value = "Server=tcp:${azurerm_sql_server.terraform-sqlserver.fully_qualified_domain_name} Database=${azurerm_sql_database.terraform-sqldatabase.name};User ID=${azurerm_sql_server.terraform-sqlserver.administrator_login};Password=${azurerm_sql_server.terraform-sqlserver.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
+  }
+}
+resource "azurerm_mssql_server" "terraform-sqlserver" {
+  name                          = "terraform-sqlserver"
+  resource_group_name           = azurerm_resource_group.RG-Terraform.name
+  location                      = azurerm_resource_group.RG-Terraform.location
+  version                       = "12.0"
+  administrator_login           = "houssem"
+  administrator_login_password  = "4-v3ry-53cr37-p455w0rd"
+  minimum_tls_version           = "1.2"
+}
+
+resource "azurerm_mssql_database" "terraform-sqldatabase" {
+  name                = "terraform-sqldatabase"
+  server_id         = azurerm_mssql_server.terraform-sqlserver.id
+
+  tags = {
+    environment = "production"
+  }
 }
